5b997b169c86449b9f0264165d15c7a76ec6dc62
gagagahuy <gagagahuy@gmail.com>
2023-02-11 16:20:15 +0700

    update source code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-    return <div className="App"></div>;
+    const [value, setValue] = useState("");
+    console.log(value);
+    return (
+        <div className="flex gap-4 justify-center items-center h-screen">
+            <input
+                type="text"
+                className="border border-blue-600 px-4 py-2 w-[400px] outline-none rounded-md"
+                onChange={(e) => setValue(e.target.value)}
+            />
+            <button
+                type="button"
+                className="outline-none bg-blue-500 rounded-md px-4 py-2 text-white text-center"
+            >
+                ADD
+            </button>
+        </div>
+    );
 }
 
 export default App;
